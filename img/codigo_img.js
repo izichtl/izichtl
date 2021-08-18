18083843ef20eccfdddb75ed369f4a8abd2e0e9a
izichtl <izichtl@gmail.com>
2021-08-18 00:17:16 -0300

    imgúpdate

--- img/codigo_img.js
+++ img/codigo_img.js
@@ -1,13 +1,14 @@
-function readMe(iz){
-    iz.nome = 'Ivan Zichtl'
-    iz.titulo = 'Desenvolvedor Junior Nodejs'
-    iz.localização = 'Nova Iguaçu - Rj'
+const iz = {
+    nome: 'Ivan Zichtl',
+    titulo: 'Desenvolvedor Junior Nodejs',
+    localização: 'Nova Iguaçu - Rj',
     
-    iz.conhecimentos =   `html, css, markdown, javascript, typescript, php, jsx, ejs,`
-                            `RegEx, Api, NodeJs, React, MongoDB, Express, Jekyll,`
-                            `CLI, Git, Github, PMBOK, Scrum, Inglês Técnico.`
-    iz.academico[0] =  `Análise de Sistemas - cursando`
-    iz.academico[1] =  `Arquitetura e Urbanismo - 2017`
+    conhecimentos:  [html, css, markdown, javascript, typescript, php, jsx, ejs,
+                     RegEx, Api, NodeJs, React, MongoDB, Express, Jekyll,
+                     CLI, Git, Github, PMBOK, Scrum, Inglês_Técnico],
+    
+    academico: [`Análise de Sistemas - cursando`, `Arquitetura e Urbanismo - 2017`],
+    games: [`civilization`, `rummikub`]
 }
 
-const leiame = readMe(iz);
+console.info(readMe(iz));
